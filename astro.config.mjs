6cc0e069186518e4538686fbd33c01a70f4822d2
Jan-Marlon Leibl <jleibl@proton.me>
2025-03-09 13:38:05 +0100

    feat: add initial Astro project structure and components

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react(),
+    icon({
+      include: {
+        ph: ['*'],
+        logos: ['*'],
+        mdi: ['*']
+      }
+    })
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
