531f2c1288e275446be12c466d129aa60a5c6d26
Jan-Marlon Leibl <jleibl@proton.me>
2025-05-16 11:56:21 +0200

    feat: add sitemap integration and update metadata settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jleibl.net",
   integrations: [
     react(),
+    sitemap(),
     icon({
       include: {
         ph: ["*"],
